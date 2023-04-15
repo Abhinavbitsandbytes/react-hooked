f3e8873a609f031fec3b3b68b4ccff28eaf4536e
Gupta <abhinav.gupta@siemens.com>
2023-04-15 17:36:14 +0530

    lec-14

--- hooked/src/components/DataFetching.js
+++ hooked/src/components/DataFetching.js
@@ -3,7 +3,13 @@ import axios from 'axios';
 
 function  DataFetching() {
 const [post, setPost] = useState({});
-const [id, setId] = useState(1)
+const [id, setId] = useState(1);
+const [idFromButtonClick, setIdFromButtonClick] = useState(1)
+
+const handleClick = () =>{
+    setIdFromButtonClick(id)
+}
+
 useEffect(()=>{
     axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then(res => {
@@ -13,10 +19,11 @@ useEffect(()=>{
     .catch(err => {
         console.log(err);
     })
-},[id])
+},[idFromButtonClick])
     return (
         <div>
             <input type='text' value={id} onChange={e => setId(e.target.value)} />
+            <button type='button' onClick={handleClick}>Fetch Post</button>
             {/* <ul>
                 {posts.map(post => <li key={post.id}>{post.title}</li>)}
             </ul> */}
