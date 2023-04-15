95aec209097ce5eae874807cfd505a9adb006305
Gupta <abhinav.gupta@siemens.com>
2023-04-16 03:29:42 +0530

    lec-16

--- hooked/src/App.js
+++ hooked/src/App.js
@@ -1,7 +1,9 @@
+import React from 'react';
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import ClassCounterOne from './components/ClassCounterOne';
 import ClassMouse from './components/ClassMouse';
+import ComponentC from './components/ComponentC';
 import DataFetching from './components/DataFetching';
 import HookCounter from './components/HookCounter';
 import HookCounterFour from './components/HookCounterFour';
@@ -13,10 +15,27 @@ import IntervalClassCounter from './components/IntervalClassCounter';
 import IntervalHookCounter from './components/IntervalHookCounter';
 import MouseContainer from './components/MouseContainer';
 
+export const UserContext = React.createContext()
+export const ChannelContext = React.createContext()
+
 function App() {
   return (
     <div className="App">
-      <DataFetching/>
+
+      <UserContext.Provider value={'Vishwas'}>
+        <ChannelContext.Provider value={'Codevolution'}>
+      <ComponentC/>
+      </ChannelContext.Provider>
+      </UserContext.Provider>
+     
+     
+     
+     
+     
+     
+     
+     
+      {/* <DataFetching/> */}
       {/* <IntervalHookCounter/> */}
       {/* <IntervalClassCounter/> */}
       {/* <MouseContainer/> */}
